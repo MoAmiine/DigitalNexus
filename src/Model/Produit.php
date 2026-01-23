a2557fac1274bbdb0727a14b763ebde27b7a622f
Moamiine <medamine.teljaoui@gmail.com>
2026-01-23 09:46:13 +0100

    Created function findByIds to show products in panier

--- src/Model/Produit.php
+++ src/Model/Produit.php
@@ -1,10 +1,13 @@
-<?php 
+<?php
+
 namespace App\Model;
+
 use App\Core\Database;
 use App\Model\Categorie;
 use PDO;
 
-class Produit {
+class Produit
+{
     private int $id;
     private string $nom;
     private int $stock;
@@ -18,7 +21,7 @@ class Produit {
     {
         $this->db = Database::dbConnect();
     }
-    
+
 
     public function getId(): int
     {
@@ -78,8 +81,9 @@ class Produit {
 
         return $this;
     }
-    public function getCategorie() {
-        return $this->categorie_nom ;
+    public function getCategorie()
+    {
+        return $this->categorie_nom;
     }
     public function setCategorie(string $categorie_nom): self
     {
@@ -88,39 +92,48 @@ class Produit {
         return $this;
     }
 
-    public function showAllProduits(){
-        $query  ='SELECT p.*, c.nom AS categorie_nom 
+    public function showAllProduits()
+    {
+        $query  = 'SELECT p.*, c.nom AS categorie_nom 
               FROM produits p 
               LEFT JOIN categories c ON p.categories_id = c.id';
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_CLASS, self::class);
     }
-    public function getProduitCount(){
+    public function getProduitCount()
+    {
         $query = 'SELECT COUNT(*) FROM produits';
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_NUM);
-
     }
 
-    public function insertProduit(){
+    public function insertProduit()
+    {
         $query = 'INSERT INTO produits(nom, prix, stock, categories_id) VALUES(:nom, :prix, :stock, :categories_id)';
         $stmt = $this->db->prepare($query);
         $stmt->execute([
             'nom' => $this->nom,
             'prix' => $this->prix,
             'stock' => $this->stock,
-            'categories_id' =>$this->categories_id
+            'categories_id' => $this->categories_id
         ]);
     }
-    public function DeleteProduit(){
+    public function DeleteProduit()
+    {
         $query = 'DELETE FROM produits WHERE id = ?';
         $stmt = $this->db->prepare($query);
         $stmt->execute([
             $this->id
         ]);
     }
-
-
+    public function findByIds(array $ids)
+    {
+        $placeholders = implode(',', array_fill(0, count($ids), '?'));
+        $query = "SELECT * FROM produits WHERE id IN ($placeholders)";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute($ids);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
 }
